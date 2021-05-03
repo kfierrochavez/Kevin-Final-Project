62fe42d05b70a722ff923d725008303b3b3cb8c8
kfierrochavez <83326871+kfierrochavez@users.noreply.github.com>
2021-05-03 10:28:35 -0500

    Add files via upload

---
+++ readmeFinalProject.m
@@ -0,0 +1,21 @@
+How to use Plant Conditions
+
+1. Run the code by either pushing run or typing "PlantConditions" into the
+command window.
+
+2. Step 1 : When the UI pop's up, you can select the month in which you would plan
+to plant your tree using the scroll bar.
+
+3. If you selected a month that is good to plant in, then it will let you
+know that the time is right, and for you to move onto the next check (Step 2).
+If the month you selected was not ideal for your tree then it will tell you
+to pick a different month.
+
+4. Step 2 : After picking the right month, you must now pick the correct zone.
+Use the radio buttons for this step.
+
+5. Every plant has a hardiness zone rating which dictates what parts of the
+country it can survive in. By picking the right zone, the UI will let you
+know that you're good to go and can plant your tree. Nebraska is zones 5-6, 
+so anything other than zones 5 and 6, the UI will tell you that the enviornment
+is not the best for your plant and to choose a different zone.
